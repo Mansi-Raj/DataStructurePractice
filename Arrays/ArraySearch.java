020c1f71e1f636644b76fbdc99d382c3361c7631
Mansi-Raj <mansi.raj.jnv@gmail.com>
2024-02-11 15:12:47 +0530

    add array files

---
+++ Arrays/ArraySearch.java
@@ -0,0 +1,27 @@
+package DataStructurePractice;
+
+import java.util.*;
+
+public class ArraySearch {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (target == nums[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        ArraySearch as = new ArraySearch();
+        int t = s.nextInt();
+        System.out.println(as.search(arr, t));
+    }
+}
