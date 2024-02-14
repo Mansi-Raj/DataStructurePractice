2ecbc8b82d36f3fd60938c0e48b0bee6b3f6564d
Mansi-Raj <mansi.raj.jnv@gmail.com>
2024-02-15 01:06:43 +0530

    add code

---
+++ Arrays/RearrangeArray.java
@@ -0,0 +1,35 @@
+package DataStructurePractice;
+
+import java.util.*;
+
+public class RearrangeArray {
+    public int[] rearrangeArray(int[]n){
+        int p = 0, m = 1;
+        int[] result = new int[n.length];
+        
+        for(int i= 0;i<result.length;i++){
+            if(n[i]>0){
+                result[p] = n[i];
+                p += 2;
+            }
+            else{
+                result[m] = n[i];
+                m += 2;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        RearrangeArray a=new RearrangeArray();
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the length of array:");
+        int n=s.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        System.out.print(a.rearrangeArray(arr));
+    }
+}
